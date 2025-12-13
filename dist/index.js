40eeee4f27a5112fba2f06f9b49510d0e2e4741e
Tom <tomervingmz@gmail.com>
2025-12-13 08:23:51 +0800

    npm run build

---
+++ dist/index.js
@@ -0,0 +1,27 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const morgan_1 = __importDefault(require("morgan"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    origin: ["http://localhost:3000", "https://docutracker.vercel.app"],
+    credentials: true,
+}));
+app.use((0, cookie_parser_1.default)('dev'));
+app.use(express_1.default.json());
+app.use((0, morgan_1.default)('dev'));
+app.get("/", (req, res) => {
+    res.send("Document tracker API is running");
+});
+//put api routes here
+const PORT = process.env.PORT || 6000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port HTTP://LOCALHOST:${PORT}`);
+});
